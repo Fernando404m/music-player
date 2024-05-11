aa7db6071a5413ab0e2ae2d39c4976fc3d2d0d25
Fernando404m <159110705+Fernando404m@users.noreply.github.com>
2024-05-11 17:05:03 -0300

    finalizado

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const audio = new Audio()
 
 var musicas = [
-    {id: 1, img: "", title: "Mirror mirror", src: "musicas/Mirror Mirror (White Trailer) (feat. Casey Lee Williams).mp3"},
-    {id: 2, img: "", title: "The weight of the world", src: "musicas/NieR_  Automata OST-The Weight of the World ENG Lyrics.mp3"},
-    {id: 3, img: "", title: "One thing", src: "musicas/One Thing (feat. Casey Lee Williams) by Jeff Williams with Lyrics.mp3"},
-    {id: 4, img: "", title: "Red like roses", src: "musicas/Red Like Roses (Red Trailer) (feat. Casey Lee Williams).mp3"},
-    {id: 5, img: "", title: "This will be the day", src: "musicas/This Will Be the Day (feat. Casey Lee Williams).mp3"},
-    {id: 6, img: "", title: "Time to say goodbye", src: "musicas/Time to Say Goodbye.mp3"}
+    {id: 1, img: "img/RWBY.png", title: "Mirror mirror", src: "musicas/Mirror Mirror (White Trailer) (feat. Casey Lee Williams).mp3"},
+    {id: 2, img: "img/nier.png", title: "The weight of the world", src: "musicas/NieR_  Automata OST-The Weight of the World ENG Lyrics.mp3"},
+    {id: 3, img: "img/RWBY-neo.png", title: "One thing", src: "musicas/One Thing (feat. Casey Lee Williams) by Jeff Williams with Lyrics.mp3"},
+    {id: 4, img: "img/RWBY.png", title: "Red like roses", src: "musicas/Red Like Roses (Red Trailer) (feat. Casey Lee Williams).mp3"},
+    {id: 5, img: "img/RWBY.png", title: "This will be the day", src: "musicas/This Will Be the Day (feat. Casey Lee Williams).mp3"},
+    {id: 6, img: "img/RWBYv2.png", title: "Time to say goodbye", src: "musicas/Time to Say Goodbye.mp3"}
 ]
 
 function placeMusic() {
@@ -28,6 +28,7 @@ function chooseMusic(id) {
             atual = musicas.find(musica => musica.id == id)
             audio.src = musica.src
             audio.play()
+            document.getElementById("img-atual").style.backgroundImage = `url(${musica.img})`
         }
     })
     document.getElementById("playpause").innerText = "pause"
@@ -38,16 +39,20 @@ function skip(dir) {
     if (dir == "proxima") {
         atual = musicas[pos + 1]
         if (atual != null) {
-            audio.src = musicas[pos + 1].src
+            var musica = musicas[pos + 1]
+            audio.src = musica.src
             audio.play()
+            document.getElementById("img-atual").style.backgroundImage = `url(${musica.img})`
         } else {
             atual = musicas[pos]
         }
     } else {
         atual = musicas[pos - 1]
         if (atual != null) {
-            audio.src = musicas[pos - 1].src
+            var musica = musicas[pos - 1]
+            audio.src = musica.src
             audio.play()
+            document.getElementById("img-atual").style.backgroundImage = `url(${musica.img})`
         } else {
             atual = musicas[pos]
         }
